aadbefc657d8f6bff8759d4afc683adc7c7800ee
gospeljourney <308952100+gospeljourney@users.noreply.github.com>
2026-07-25 23:28:13 +0900

    style: verify Korean home foundation gate (Refs #6)

---
+++ docs/.vitepress/theme/KoreanHome.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Content, useData } from 'vitepress'
+import {
+  VPHomeContent,
+  VPHomeFeatures,
+  VPHomeHero
+} from 'vitepress/theme'
+
+const { frontmatter, theme } = useData()
+</script>
+
+<template>
+  <main
+    class="VPHome"
+    :class="{
+      'external-link-icon-enabled': theme.externalLinkIcon
+    }"
+  >
+    <VPHomeHero />
+    <VPHomeFeatures />
+    <VPHomeContent v-if="frontmatter.markdownStyles !== false">
+      <Content />
+    </VPHomeContent>
+    <Content v-else />
+  </main>
+</template>
